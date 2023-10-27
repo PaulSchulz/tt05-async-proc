11a68c89f04dfdf60e9156519584c58ac47bcd88
Paul Schulz <paul@mawsonlakes.org>
2023-10-27 21:11:35 +1030

    Modified testbench file to use new top module

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+    tt_um_async_proc_paulschulz tt_um_async_proc_paulschulz (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
