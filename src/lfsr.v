f540cab7810bbe1aa66ec5be04969e0551207be1
Paul Schulz <paul@mawsonlakes.org>
2023-10-29 10:09:04 +1030

    Added guard code back into (ifndef/define and endif) to module
    Verilog built but test action in github failed.

--- src/lfsr.v
+++ src/lfsr.v
@@ -1,11 +1,13 @@
 // Configurable Linear Feedback Shift Register.
+`ifndef LFSR_V
+`define LFSR_V
 
 module lfsr(clk, reset, enable, lfsr);
 
     parameter TAPS   = 16'b0000000000011101;	// bitmask for taps
     parameter INVERT = 0;		                // invert feedback bit?
     localparam NBITS = $size(TAPS);            // bit width (derived from TAPS)
-  
+
     input clk, reset;
     input enable;			      // only perform shift when enable=1
     output reg [NBITS-1:0] lfsr;  // shift register
@@ -21,3 +23,5 @@ module lfsr(clk, reset, enable, lfsr);
     end
 
 endmodule
+
+`endif
