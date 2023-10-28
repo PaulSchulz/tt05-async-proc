7ebba8c1a2b78e2b6da59b9e8de067e7f6f201d9
Paul Schulz <paul@mawsonlakes.org>
2023-10-28 20:03:39 +1030

    Cleanup and debug

--- src/lif.v
+++ src/lif.v
@@ -5,30 +5,19 @@ module lif (
     input  wire [3:0] in4,
     output wire [3:0] out,
     
-//    input  wire [7:0] current,
-//    output wire [7:0] next_state,
-//    output wire       spike,
-    
     input  wire       clk,
     input  wire       rst_n
-    
-);
+    );
 
-    // reg [7:0] state, threshold;
     reg [3:0] state;
     
-    // resting potential and threshold
-    // assign next_state = current + (state >> 1);
-    // assign spike = (state >= threshold);
-
     assign out = state;
 
     always @(posedge clk) begin
        if (!rst_n) begin
            state <= 0;
-           //threshold <= 32;
        end else begin
-           state <= {D[0],C[0],B[0],A[0]};
+           state <= {in4[0],in3[0],in2[0],in1[0]};
        end 
 
     end
